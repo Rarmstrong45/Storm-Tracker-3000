1519d1048914d807e326979330b70e32fdc2b2c3
Rarmstrong45 <Ryan_armstrong07@yahoo.com>
2021-04-30 19:08:05 -0500

    started JS added variable and API key

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+//Declare a variable to store the searched city
+var city="";
+// variable declaration
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
+
+// API Key
+var APIKey="a0aca8a89948154a4182dcecc780b513";
+// search to see if city is in storage
+function find(c){
+    for (var i=0; i<sCity.length; i++){
+        if(c.toUpperCase()===sCity[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
+
